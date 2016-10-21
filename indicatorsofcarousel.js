00a59a83a4c8509d6e5595553321ba6cb1a4b2c7
Veljko Stefanovic <veljkos82@gmail.com>
2016-10-21 22:31:24 +0200

    My Bootstrap Carousel alternative.

---
+++ indicatorsofcarousel.js
@@ -0,0 +1,191 @@
+		
+		var clicksRight=0;
+		var clicksLeft=0;
+		var trini=0;
+		var gog=0;
+		var pret=0;
+		var x=0;
+		var y=0;
+		var br=0;
+		var index=0;
+		var currentIndex=0;
+		var clicks=0;
+		var num=0;
+		var mr=0;
+		var tmp=0;
+		var OldValue = 0;
+$( document ).ready(function() {
+
+	var prTop=$(".con1").offset().top+$(".con1").outerHeight()-$(".pr").outerHeight();
+	$(".pr").offset({top: prTop});
+
+  $( ".sl" ).click(function() { //Klikom na element sa class="sl" ..
+	gog=0;
+	var width=$( ".sl" ).outerWidth();
+	var corr=(width/100)*4.4;
+	width+=corr;
+	index = $( ".sl" ).index( this ); //Index(trenutni) <li> elementa na koji je kliknuto.
+	var count = $('.item').length; // je isto kao document.getElementById("pics").children.length
+	var cnt = $('.sl').length;
+	
+	document.getElementById("mix1").innerHTML=index;
+
+	if(index!=0){
+		currentIndex=index;
+		clicksLeft=count-(count-currentIndex);
+	}
+	
+	if(index>pret){
+		
+		if(index==3){
+			gog=40;
+		}
+		
+		if(index>3){
+			gog=(width*(index-3))+40;
+		}
+		
+		$(".con1").animate({scrollLeft: gog});
+		pret=index;
+	}
+	
+	if(index<pret){
+		
+		if(index==3){
+			gog=40;
+		}
+		
+		if(index>3){
+			gog=(width*(index-3))+40;
+		}
+		
+		$(".con1").animate({scrollLeft: gog});
+		pret=index;
+	}
+	
+	
+	x=$(".sl").eq(index).offset().top; // TOP pozicija indeksiranog elementa na koji je kliknuto.
+	y=$(".sl").eq(index).offset().left; //LEFT pozicija indeksiranog elementa na koji je kliknuto.
+	
+	br=$(".con1").scrollLeft();
+	clicksLeft=count-index;
+	document.getElementById("mix").innerHTML=clicksLeft;
+});
+
+
+
+});
+
+
+
+function right(){
+	gog=0;
+	br=$(".con1").scrollLeft();
+	var width=$( ".sl" ).outerWidth();
+	currentIndex = $('div.active').index() + 1;
+	var count = $('.item').length; // je isto kao document.getElementById("pics").children.length
+	var cnt = $('.sl').length;
+	var corr=(width/100)*4.4;
+	width+=corr;
+	var p1=document.getElementById("lista").scrollWidth;
+	var p2=$("#lista").outerWidth();
+	var p3=p1-p2;//4196-720=3476 -> Rezultat mi treba :)
+	clicksRight+=1;
+	
+	if(br==0 && currentIndex>2){
+		gog=br+((width-15)/2);
+		$(".con1").scrollLeft(gog); //Nikako ovako $(".con1").animate({scrollLeft: gog}); jer brouser ne stigne da registruje klikove pa dolazi do zbunjivanja istog i kasnjenja.
+	}
+	
+	if(br>0){
+		gog=br+width;
+		$(".con1").scrollLeft(gog);
+	}
+	
+	if(br==p3 && currentIndex==count){
+		gog=0;
+		$(".con1").scrollLeft(gog);
+		clicksRight=0;
+	}
+
+	clicksLeft=count-currentIndex;
+	document.getElementById("mix").innerHTML=clicksLeft;
+	document.getElementById("mix1").innerHTML=currentIndex;
+	
+	
+}
+
+
+function left(){
+	gog==0;
+	br=$(".con1").scrollLeft();
+	var width=$( ".sl" ).outerWidth();
+	var count = $('.item').length; // je isto kao document.getElementById("pics").children.length
+	var cnt = $('.sl').length;
+	var corr=(width/100)*4;
+	width+=corr;
+	var p1=document.getElementById("lista").scrollWidth;
+	var p2=$("#lista").outerWidth();
+	var p3=p1-p2;//4196-720=3476 -> Rezultat mi treba :)
+	clicksLeft+=1;
+
+	if(br==0 && clicksLeft==1){
+		gog=p3;
+		$(".con1").scrollLeft(gog);
+		//alert("1");
+		currentIndex=count;
+	}
+	
+	if(clicksLeft>4 && br==p3){
+		gog=br-width;
+		$(".con1").scrollLeft(gog);
+		//alert("2");
+	}
+	
+	if(clicksLeft>4){
+		gog=br-width;
+		$(".con1").scrollLeft(gog);
+		//alert("3");
+	}
+
+	if(currentIndex==0 && clicksRight>0){ //Ovo vazi samo ako je kliktano na desnu strelicu
+		gog=p3;
+		$(".con1").scrollLeft(gog);
+		//alert("4");
+	}
+	
+	if(clicksLeft>4 && clicksRight>0){ //Ovo vazi samo ako je kliktano na desnu strelicu
+		gog=br-width;
+		$(".con1").scrollLeft(gog);
+		//alert("5");
+	}
+
+	
+	if(clicksRight>0){
+		currentIndex-=1;	
+		//alert("6");
+	}
+	
+	if(clicksRight==0 && currentIndex>=0){
+		currentIndex=count-clicksLeft;	
+		//alert("7");
+	}
+	
+	if(currentIndex==count && clicksLeft){
+			gog=p3;
+			$(".con1").scrollLeft(gog);
+			//alert("8");
+	}
+	
+	if(currentIndex==0){
+		clicksLeft=0;
+		//alert("9");
+	}
+	
+
+	document.getElementById("mix").innerHTML=clicksLeft;
+	document.getElementById("mix1").innerHTML=currentIndex;
+}
+
+
+
---
+++ indicatorsofcarousel.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+  <script src="indicatorsofcarousel.js"></script>
+  <link rel="stylesheet" type="text/css" href="indicatorsofcarousel.css">
+
+
+</head>
+<body>
+
+<div class="container text-center">
+
+  <br>
+  <div id="myCarousel" class="carousel" data-ride="carousel" data-interval="false"><!-- Ako se ukloni .slide klasa onda nema animacije i koliko god da se brzo klikce na strelice karusel i donji slajdovi se savrseno poravnjaju. -->
+  
+
+    <!-- Wrapper for slides -->
+    <div id="pics" class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="img/img1.jpg" alt="Chania"><br><span class="toggle">0</span>
+      </div>
+
+      <div class="item">
+        <img src="img/img2.jpg" alt="Chania"><br><span class="toggle">1</span>
+      </div>
+    
+      <div class="item">
+        <img src="img/img3.jpg" alt="Flower"><br><span class="toggle">2</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img4.jpg" alt="Flower"><br><span class="toggle">3</span>
+      </div>
+
+      <div class="item">
+        <img src="img/img5.jpg" alt="Flower"><br><span class="toggle">4</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img6.jpg" alt="Flower"><br><span class="toggle">5</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img7.jpg" alt="Chania"><br><span class="toggle">6</span>
+      </div>
+
+      <div class="item">
+        <img src="img/img8.jpg" alt="Chania"><br><span class="toggle">7</span>
+      </div>
+    
+      <div class="item">
+        <img src="img/img9.jpg" alt="Flower"><br><span class="toggle">8</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img10.jpg" alt="Flower"><br><span class="toggle">9</span>
+      </div>
+
+      <div class="item">
+        <img src="img/img11.jpg" alt="Flower"><br><span class="toggle">10</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img12.jpg" alt="Flower"><br><span class="toggle">11</span>
+      </div>
+	  
+	   <div class="item">
+        <img src="img/img13.jpg" alt="Flower"><br><span class="toggle">12</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img14.jpg" alt="Flower"><br><span class="toggle">13</span>
+      </div>
+
+      <div class="item">
+        <img src="img/img15.jpg" alt="Flower"><br><span class="toggle">14</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img16.jpg" alt="Flower"><br><span class="toggle">15</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img17.jpg" alt="Chania"><br><span class="toggle">16</span>
+      </div>
+
+      <div class="item">
+        <img src="img/img18.jpg" alt="Chania"><br><span class="toggle">17</span>
+      </div>
+    
+      <div class="item">
+        <img src="img/img19.jpg" alt="Flower"><br><span class="toggle">18</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img20.jpg" alt="Flower"><br><span class="toggle">19</span>
+      </div>
+	  
+	  <div class="item">
+        <img src="img/img21.jpg" alt="Flower"><br><span class="toggle">20</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img22.jpg" alt="Flower"><br><span class="toggle">21</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img23.jpg" alt="Flower"><br><span class="toggle">22</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img24.jpg" alt="Flower"><br><span class="toggle">23</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img25.jpg" alt="Flower"><br><span class="toggle">24</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img26.jpg" alt="Flower"><br><span class="toggle">25</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img27.jpg" alt="Flower"><br><span class="toggle">26</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img28.jpg" alt="Flower"><br><span class="toggle">27</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img29.jpg" alt="Flower"><br><span class="toggle">28</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img30.jpg" alt="Flower"><br><span class="toggle">29</span>
+      </div>
+	   
+	  <div class="item">
+        <img src="img/img31.jpg" alt="Flower"><br><span class="toggle">30</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img32.jpg" alt="Flower"><br><span class="toggle">31</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img33.jpg" alt="Flower"><br><span class="toggle">32</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img34.jpg" alt="Flower"><br><span class="toggle">33</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img35.jpg" alt="Flower"><br><span class="toggle">34</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img36.jpg" alt="Flower"><br><span class="toggle">35</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img37.jpg" alt="Flower"><br><span class="toggle">36</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img38.jpg" alt="Flower"><br><span class="toggle">37</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img39.jpg" alt="Flower"><br><span class="toggle">38</span>
+      </div>
+	    
+	  <div class="item">
+        <img src="img/img40.jpg" alt="Flower"><br><span class="toggle">39</span>
+      </div>
+	  
+    </div>
+
+    <!-- Left and right controls -->
+    <a id="prev" onclick="left()" class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span  class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a  id="next" onclick="right()" class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+	
+  </div>
+  <div class="text-center">
+	<div class="pr"></div>
+	<div id="rot" class="lnk">
+	
+	
+	<ol id="lista" class="faceless con1">
+    <li class="ord_list_elem" data-target="#myCarousel" data-slide-to="0"><a href="#"><img class="sl" src="img/img1.jpg" alt="Chania"><br><span class="toggle">0</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="1"><a href="#"><img class="sl" src="img/img2.jpg" alt="Chania"><br><span class="toggle">1</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="2"><a href="#"><img class="sl" src="img/img3.jpg" alt="Flower"><br><span class="toggle">2</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="3"><a href="#"><img class="sl" src="img/img4.jpg" alt="Flower"><br><span class="toggle">3</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="4"><a href="#"><img class="sl" src="img/img5.jpg" alt="Flower"><br><span class="toggle">4</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="5"><a href="#"><img class="sl" src="img/img6.jpg" alt="Flower"><br><span class="toggle">5</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="6"><a href="#"><img class="sl" src="img/img7.jpg" alt="Chania"><br><span class="toggle">6</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="7"><a href="#"><img class="sl" src="img/img8.jpg" alt="Chania"><br><span class="toggle">7</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="8"><a href="#"><img class="sl" src="img/img9.jpg" alt="Flower"><br><span class="toggle">8</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="9"><a href="#"><img class="sl" src="img/img10.jpg" alt="Flower"><br><span class="toggle">9</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="10"><a href="#"><img class="sl" src="img/img11.jpg" alt="Flower"><br><span class="toggle">10</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="11"><a href="#"><img class="sl" src="img/img12.jpg" alt="Flower"><br><span class="toggle">11</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="12"><a href="#"><img class="sl" src="img/img13.jpg" alt="Flower"><br><span class="toggle">12</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="13"><a href="#"><img class="sl" src="img/img14.jpg" alt="Flower"><br><span class="toggle">13</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="14"><a href="#"><img class="sl" src="img/img15.jpg" alt="Flower"><br><span class="toggle">14</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="15"><a href="#"><img class="sl" src="img/img16.jpg" alt="Flower"><br><span class="toggle">15</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="16"><a href="#"><img class="sl" src="img/img17.jpg" alt="Flower"><br><span class="toggle">16</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="17"><a href="#"><img class="sl" src="img/img18.jpg" alt="Flower"><br><span class="toggle">17</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="18"><a href="#"><img class="sl" src="img/img19.jpg" alt="Flower"><br><span class="toggle">18</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="19"><a href="#"><img class="sl" src="img/img20.jpg" alt="Flower"><br><span class="toggle">19</span></a></li>
+	
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="20"><a href="#"><img class="sl" src="img/img21.jpg" alt="Flower"><br><span class="toggle">20</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="21"><a href="#"><img class="sl" src="img/img22.jpg" alt="Flower"><br><span class="toggle">21</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="22"><a href="#"><img class="sl" src="img/img23.jpg" alt="Flower"><br><span class="toggle">22</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="23"><a href="#"><img class="sl" src="img/img24.jpg" alt="Flower"><br><span class="toggle">23</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="24"><a href="#"><img class="sl" src="img/img25.jpg" alt="Flower"><br><span class="toggle">24</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="25"><a href="#"><img class="sl" src="img/img26.jpg" alt="Flower"><br><span class="toggle">25</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="26"><a href="#"><img class="sl" src="img/img27.jpg" alt="Flower"><br><span class="toggle">26</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="27"><a href="#"><img class="sl" src="img/img28.jpg" alt="Flower"><br><span class="toggle">27</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="28"><a href="#"><img class="sl" src="img/img29.jpg" alt="Flower"><br><span class="toggle">28</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="29"><a href="#"><img class="sl" src="img/img30.jpg" alt="Flower"><br><span class="toggle">29</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="30"><a href="#"><img class="sl" src="img/img31.jpg" alt="Flower"><br><span class="toggle">30</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="31"><a href="#"><img class="sl" src="img/img32.jpg" alt="Flower"><br><span class="toggle">31</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="32"><a href="#"><img class="sl" src="img/img33.jpg" alt="Flower"><br><span class="toggle">32</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="33"><a href="#"><img class="sl" src="img/img34.jpg" alt="Flower"><br><span class="toggle">33</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="34"><a href="#"><img class="sl" src="img/img35.jpg" alt="Flower"><br><span class="toggle">34</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="35"><a href="#"><img class="sl" src="img/img36.jpg" alt="Flower"><br><span class="toggle">35</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="36"><a href="#"><img class="sl" src="img/img37.jpg" alt="Flower"><br><span class="toggle">36</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="37"><a href="#"><img class="sl" src="img/img38.jpg" alt="Flower"><br><span class="toggle">37</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="38"><a href="#"><img class="sl" src="img/img39.jpg" alt="Flower"><br><span class="toggle">38</span></a></li>
+	<li class="ord_list_elem" data-target="#myCarousel" data-slide-to="39"><a href="#"><img class="sl" src="img/img40.jpg" alt="Flower"><br><span class="toggle">39</span></a></li>
+	</ol>
+	
+	
+	</div>
+	 
+</div>
+
+</div><br>
+
+<div>currentIndex: <span id="mix1"></span></div>
+<div>clicksLeft: <span id="mix">0</span></div>
+</body>
+</html>
+
