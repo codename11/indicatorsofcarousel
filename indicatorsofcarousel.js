d95645d2dfead2c657358f56cda070c67a705fd6
Veljko Stefanovic <veljkos82@gmail.com>
2016-11-26 18:59:19 +0100

    Additional tweaks

--- indicatorsofcarousel.js
+++ indicatorsofcarousel.js
@@ -14,7 +14,7 @@
 		var mr=0;
 		var tmp=0;
 		var OldValue = 0;
-		
+		var prev_ind=0;
 function resizer() {
     
 	var prTop=$(".con1").offset().top+$(".con1").outerHeight()-$(".pr").outerHeight();
@@ -27,16 +27,14 @@ function resizer() {
 }
 $( document ).ready(function() {
 
-  $( ".sl" ).click(function() { //Klikom na element sa class="sl" ..
+  $( ".sl" ).click(function() { 
 	gog=0;
 	var width=$( ".sl" ).outerWidth();
 	var corr=(width/100)*4.4;
 	width+=corr;
-	index = $( ".sl" ).index( this ); //Index(trenutni) <li> elementa na koji je kliknuto.
-	var count = $('.item').length; // je isto kao document.getElementById("pics").children.length
+	index = $( ".sl" ).index( this ); 
+	var count = $('.item').length; 
 	var cnt = $('.sl').length;
-	
-	document.getElementById("mix1").innerHTML=index;
 
 	if(index!=0){
 		currentIndex=index;
@@ -72,12 +70,12 @@ $( document ).ready(function() {
 	}
 	
 	
-	x=$(".sl").eq(index).offset().top; // TOP pozicija indeksiranog elementa na koji je kliknuto.
-	y=$(".sl").eq(index).offset().left; //LEFT pozicija indeksiranog elementa na koji je kliknuto.
+	x=$(".sl").eq(index).offset().top; 
+	y=$(".sl").eq(index).offset().left;
 	
 	br=$(".con1").scrollLeft();
 	clicksLeft=count-index;
-	document.getElementById("mix").innerHTML=clicksLeft;
+
 });
 
 
@@ -91,18 +89,18 @@ function right(){
 	br=$(".con1").scrollLeft();
 	var width=$( ".sl" ).outerWidth();
 	currentIndex = $('div.active').index() + 1;
-	var count = $('.item').length; // je isto kao document.getElementById("pics").children.length
+	var count = $('.item').length;
 	var cnt = $('.sl').length;
 	var corr=(width/100)*4.4;
 	width+=corr;
 	var p1=document.getElementById("lista").scrollWidth;
 	var p2=$("#lista").outerWidth();
-	var p3=p1-p2;//4196-720=3476 -> Rezultat mi treba :)
+	var p3=p1-p2;
 	clicksRight+=1;
 	
 	if(br==0 && currentIndex>2){
 		gog=br+((width-15)/2);
-		$(".con1").scrollLeft(gog); //Nikako ovako $(".con1").animate({scrollLeft: gog}); jer brouser ne stigne da registruje klikove pa dolazi do zbunjivanja istog i kasnjenja.
+		$(".con1").scrollLeft(gog); 
 	}
 	
 	if(br>0){
@@ -117,9 +115,6 @@ function right(){
 	}
 
 	clicksLeft=count-currentIndex;
-	document.getElementById("mix").innerHTML=clicksLeft;
-	document.getElementById("mix1").innerHTML=currentIndex;
-	
 	
 }
 
@@ -128,71 +123,60 @@ function left(){
 	gog==0;
 	br=$(".con1").scrollLeft();
 	var width=$( ".sl" ).outerWidth();
-	var count = $('.item').length; // je isto kao document.getElementById("pics").children.length
+	var count = $('.item').length;
 	var cnt = $('.sl').length;
 	var corr=(width/100)*4;
 	width+=corr;
 	var p1=document.getElementById("lista").scrollWidth;
 	var p2=$("#lista").outerWidth();
-	var p3=p1-p2;//4196-720=3476 -> Rezultat mi treba :)
+	var p3=p1-p2;
 	clicksLeft+=1;
 
 	if(br==0 && clicksLeft==1){
 		gog=p3;
 		$(".con1").scrollLeft(gog);
-		//alert("1");
 		currentIndex=count;
 	}
 	
 	if(clicksLeft>4 && br==p3){
 		gog=br-width;
 		$(".con1").scrollLeft(gog);
-		//alert("2");
 	}
 	
 	if(clicksLeft>4){
 		gog=br-width;
 		$(".con1").scrollLeft(gog);
-		//alert("3");
 	}
 
-	if(currentIndex==0 && clicksRight>0){ //Ovo vazi samo ako je kliktano na desnu strelicu
+	if(currentIndex==0 && clicksRight>0){ 
 		gog=p3;
 		$(".con1").scrollLeft(gog);
-		//alert("4");
 	}
 	
-	if(clicksLeft>4 && clicksRight>0){ //Ovo vazi samo ako je kliktano na desnu strelicu
+	if(clicksLeft>4 && clicksRight>0){
 		gog=br-width;
 		$(".con1").scrollLeft(gog);
-		//alert("5");
 	}
 
 	
 	if(clicksRight>0){
 		currentIndex-=1;	
-		//alert("6");
 	}
 	
 	if(clicksRight==0 && currentIndex>=0){
 		currentIndex=count-clicksLeft;	
-		//alert("7");
 	}
 	
 	if(currentIndex==count && clicksLeft){
-			gog=p3;
-			$(".con1").scrollLeft(gog);
-			//alert("8");
+		gog=p3;
+		$(".con1").scrollLeft(gog);
+			
 	}
 	
 	if(currentIndex==0){
-		clicksLeft=0;
-		//alert("9");
+		clicksLeft=0;	
 	}
 	
-
-	document.getElementById("mix").innerHTML=clicksLeft;
-	document.getElementById("mix1").innerHTML=currentIndex;
 }
 
 
